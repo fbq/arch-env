d957e644e84861b9230d2e9981bebc1dc65bf025
Boqun Feng <boqun.feng@gmail.com>
2014-04-15 00:36:28 +0800

    xmonad.hs: add two key bindings
    Ctrl-Meta(Win)-L for (L)ocking the screen
    Ctrl-Meta(Win)-B for opening the chromium (B)rowser
    
    Signed-off-by: Boqun Feng <boqun.feng@gmail.com>

--- xmonad.hs
+++ xmonad.hs
@@ -1,8 +1,12 @@
 import XMonad
+import XMonad.Util.EZConfig(additionalKeys)
 main = do
   xmonad $ defaultConfig 
     {
       terminal = "urxvt",
       modMask = mod4Mask,
       borderWidth = 3
-    }
+    } `additionalKeys` -- make sure `cabal install xmonad-contrib` first
+    [ ((mod4Mask .|. shiftMask, xK_l), spawn "xlock")
+    , ((mod4Mask .|. shiftMask, xK_b), spawn "chromium")     
+    ]
