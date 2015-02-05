49fdbb44094535228622cac1d1b8f72679d5940e
Boqun Feng <boqun.feng@gmail.com>
2015-02-05 11:20:04 +0800

    X: add new hotkey and set urxvt in daemon-client mode
    Signed-off-by: Boqun Feng <boqun.feng@gmail.com>

--- X/xmonad.hs
+++ X/xmonad.hs
@@ -1,12 +1,24 @@
+-- make sure xmonad-contrib is installed
+
 import XMonad
 import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
 main = do
-  xmonad $ defaultConfig 
-    {
-      terminal = "urxvt",
-      modMask = mod4Mask,
-      borderWidth = 3
-    } `additionalKeys` -- make sure `cabal install xmonad-contrib` first
-    [ ((mod4Mask .|. shiftMask, xK_l), spawn "xlock")
-    , ((mod4Mask .|. shiftMask, xK_b), spawn "chromium")     
+    xmonad $ ewmh defaultConfig
+        { terminal = "urxvtc"
+        , modMask = mod4Mask
+        , manageHook = myManageHook
+        , borderWidth = 1
+        , normalBorderColor = "navy"
+        , focusedBorderColor = "gray"
+        , handleEventHook = handleEventHook defaultConfig <+> fullscreenEventHook
+        } `additionalKeys`
+        [ ((mod4Mask .|. shiftMask, xK_l), spawn "xlock")
+        , ((mod4Mask .|. shiftMask, xK_b), spawn "chromium")
+        , ((mod4Mask .|. shiftMask, xK_s), spawn "pavucontrol")
+        ]
+
+myManageHook = composeAll
+    [ className =? "Pavucontrol" --> doFloat
     ]
