05537c95a544581cae103358dd2e0dc7bd9bc13d
Boqun Feng <boqun.feng@gmail.com>
2015-02-05 12:14:28 +0800

    X: add scratchpad terminal for urxvt

--- X/xmonad.hs
+++ X/xmonad.hs
@@ -3,12 +3,17 @@
 import XMonad
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Hooks.DynamicLog
+-- for full-screen ability of some gtk apps
 import XMonad.Hooks.EwmhDesktops
+-- for scratchpad
+import XMonad.Util.Scratchpad
+import XMonad.StackSet
+
 main = do
     xmonad $ ewmh defaultConfig
         { terminal = "urxvtc"
         , modMask = mod4Mask
-        , manageHook = myManageHook
+        , manageHook = myManageHook <+> myScratchpadHook
         , borderWidth = 1
         , normalBorderColor = "navy"
         , focusedBorderColor = "gray"
@@ -17,8 +22,12 @@ main = do
         [ ((mod4Mask .|. shiftMask, xK_l), spawn "xlock")
         , ((mod4Mask .|. shiftMask, xK_b), spawn "chromium")
         , ((mod4Mask .|. shiftMask, xK_s), spawn "pavucontrol")
+        , ((mod4Mask , xK_s), scratchpadSpawnActionTerminal "urxvtc")
         ]
 
 myManageHook = composeAll
     [ className =? "Pavucontrol" --> doFloat
     ]
+
+-- scratchpad settings
+myScratchpadHook = scratchpadManageHook $ RationalRect 0.2 0.2 0.6 0.6
